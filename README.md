39d2475229125f26b4e120decfcc30380859b812
Marian Šámal <margotka000@gmail.com>
2020-09-01 20:36:40 +0200

    add script, update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bakalari-disable-timeout
+
 Disable the auto-logoff "feature" of Bakaláři
+
+
+## Installation
+
+- Install a userscript manager, eg. [Tampermonkey for Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) or [Tampermonkey for Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/) or [Greasemonkey for Firefox](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
+- Install the script from [here](https://raw.githubusercontent.com/mariansam/bakalari-disable-timeout/master/bakalari-disable-timeout.user.js)
+- 👍
---
+++ bakalari-disable-timeout.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Bakaláři - Disable Timeout
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Disable the auto-logoff "feature" of Bakaláři
+// @author       Marian Šámal
+// @include      /^https?:\/\/.*baka.*$/
+// @exclude      https://github.com/*
+// @grant        none
+// @homepage     https://github.com/mariansam/bakalari-disable-timeout
+// @updateURL    https://raw.githubusercontent.com/mariansam/bakalari-disable-timeout/master/bakalari-disable-timeout.user.js
+// @downloadURL  https://raw.githubusercontent.com/mariansam/bakalari-disable-timeout/master/bakalari-disable-timeout.user.js
+// @supportURL   https://github.com/mariansam/bakalari-disable-timeout/issues
+// @icon         https://avatars0.githubusercontent.com/u/44744799?s=32&v=4
+// @icon64       https://avatars0.githubusercontent.com/u/44744799?s=64&v=4
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('bakalari-disable-timeout: started');
+
+    if (typeof appRoot === 'string') {
+        setInterval(() => {
+            fetch(appRoot + 'sessionextend')
+                .then(() => console.log('bakalari-disable-timeout: session extended'));
+        }, 30000);
+    }
+})();
