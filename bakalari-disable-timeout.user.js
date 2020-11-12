7659adb70cec173633938fdcb76218defa344f93
Marian Šámal <margotka000@gmail.com>
2020-11-12 19:49:40 +0100

    Bump version just to be sure

--- bakalari-disable-timeout.user.js
+++ bakalari-disable-timeout.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bakaláři - Disable Timeout
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Disable the auto-logoff "feature" of Bakaláři
 // @author       Marian Šámal
 // @include      /^https?:\/\/.*baka.*$/
